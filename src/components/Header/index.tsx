224e636450fe2d84e62b5573302015ee488a0017
Luis André <luis.oliveira10031992@gmail.com>
2021-06-14 19:05:25 -0300

    Creation of components: Header and Banner

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import { Flex, Grid, Image } from "@chakra-ui/react";
+
+export function Header() {
+  return (
+    <Flex
+      bg="white"
+      w="100%"
+      as="header"
+      mx="auto"
+      px="1rem"
+      h={["50px", "100px"]}
+      align="center"
+      justify="center"
+    >
+      <Grid
+        h="100%"
+        mx="auto"
+        w="100%"
+        maxW="1160px"
+        alignItems="center"
+        templateColumns="repeat(3, 1fr)"
+        justifyContent="center"
+      >
+        <Image
+          w={["81px", "184px"]}
+          src="/logo.svg"
+          alt="Um avião voando sobre o nome da marca World Trip"
+          justifySelf="center"
+          gridColumn="2"
+        />
+      </Grid>
+    </Flex>
+  );
+}

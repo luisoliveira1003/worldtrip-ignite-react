e724594b0e73aef1f7b1517b9d9f09de631f9e77
Luis André <luis.oliveira10031992@gmail.com>
2021-06-25 20:41:02 -0300

    :tada: Challenge Complementary Completed :rocket:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { theme } from "../styles/theme";
 
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+
+import "../styles/slider.scss";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
+import React from "react";
 import { Banner } from "../components/Banner";
+import { Characteristics } from "../components/Characteristics";
 import { Header } from "../components/Header";
+import { Separator } from "../components/Separator";
+import { Slider } from "../components/Slider";
 
 export default function Home() {
   return (
     <Flex direction="column">
       <Header />
       <Banner />
+      <Characteristics />
+      <Separator />
+
+      <Heading
+        textAlign="center"
+        fontWeight="500"
+        mb={["5", "14"]}
+        fontSize={["lg", "3xl", "4xl"]}
+      >
+        Vamos nessa?
+        <br />
+        Então escolha seu continente
+      </Heading>
+
+      <Slider />
     </Flex>
   );
 }

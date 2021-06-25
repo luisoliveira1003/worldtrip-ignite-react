cbedb9a5ca9a3d4cfc9472e69e27e2727e734a01
Luis André <luis.oliveira10031992@gmail.com>
2021-06-25 20:38:51 -0300

    Finishing the Slider component

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,99 @@
+import { Flex, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Link from "next/link";
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+export function Slider() {
+  return (
+    <Flex
+      w="100%"
+      maxW="1240px"
+      mx="auto"
+      mb={["5", "10"]}
+      h={["250px", "450px"]}
+    >
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 4000,
+        }}
+        style={{ width: "100%", flex: "1" }}
+      >
+        <SwiperSlide>
+          <Flex
+            w="100%"
+            h="100%"
+            align="center"
+            justify="center"
+            direction="column"
+            bgImage="url(/images/europe.svg)"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            textAlign="center"
+            bgPosition="center"
+          >
+            <Link href="/continent/europe">
+              <a>
+                <Heading
+                  fontSize={["3xl", "4xl", "5xl"]}
+                  color="gray.100"
+                  fontWeight="bold"
+                >
+                  Europa
+                </Heading>
+                <Text
+                  fontWeight="bold"
+                  color="gray.300"
+                  fontSize={["0.8rem", "1xl", "2xl"]}
+                  mt={["2", "4"]}
+                >
+                  O continente mais antigo.
+                </Text>
+              </a>
+            </Link>
+          </Flex>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <Flex
+            w="100%"
+            h="100%"
+            align="center"
+            justify="center"
+            direction="column"
+            bgImage="url(/images/europe.svg)"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            textAlign="center"
+            bgPosition="center"
+          >
+            <Link href="/continent/europe">
+              <a>
+                <Heading
+                  fontSize={["3xl", "4xl", "5xl"]}
+                  color="gray.100"
+                  fontWeight="bold"
+                >
+                  Europa
+                </Heading>
+                <Text
+                  fontWeight="bold"
+                  color="gray.300"
+                  fontSize={["0.8rem", "1xl", "2xl"]}
+                  mt={["2", "4"]}
+                >
+                  O continente mais antigo.
+                </Text>
+              </a>
+            </Link>
+          </Flex>
+        </SwiperSlide>
+      </Swiper>
+    </Flex>
+  );
+}

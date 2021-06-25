b5f81eaab8bb82431d29f9646316573df9c7bb55
Luis André <luis.oliveira10031992@gmail.com>
2021-06-25 20:37:57 -0300

    Finishing the Content component

---
+++ src/components/Content/Infos.tsx
@@ -0,0 +1,79 @@
+import {
+  Flex,
+  Heading,
+  Text,
+  Popover,
+  PopoverTrigger,
+  Icon,
+  PopoverContent,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+} from "@chakra-ui/react";
+import { RiInformationLine } from "react-icons/ri";
+
+export function Infos() {
+  return (
+    <Flex align="center" justify="space-between">
+      <Flex
+        direction="column"
+        justify="center"
+        align={["flex-start", "flex-start", "center"]}
+      >
+        <Heading fontSize={["2xl", "5xl"]} color="yellow.400" fontWeight="500">
+          50
+        </Heading>
+        <Text fontWeight="600" fontSize={["md", "xl"]} color="gray.700">
+          países
+        </Text>
+      </Flex>
+
+      <Flex
+        direction="column"
+        justify="center"
+        align={["flex-start", "flex-start", "center"]}
+      >
+        <Heading fontSize={["2xl", "5xl"]} color="yellow.400" fontWeight="500">
+          60
+        </Heading>
+        <Text fontWeight="600" fontSize={["md", "xl"]} color="gray.700">
+          línguas
+        </Text>
+      </Flex>
+
+      <Flex
+        direction="column"
+        justify="center"
+        align={["flex-start", "flex-start", "center"]}
+      >
+        <Heading fontSize={["2xl", "5xl"]} color="yellow.400" fontWeight="500">
+          27
+        </Heading>
+        <Text fontWeight="600" fontSize={["md", "xl"]} color="gray.700">
+          cidades +100
+          <Popover>
+            <PopoverTrigger>
+              <span>
+                <Icon
+                  cursor="pointer"
+                  as={RiInformationLine}
+                  ml="1"
+                  color="gray.400"
+                  w={["10px", "16px"]}
+                  h={["10px", "16px"]}
+                />
+              </span>
+            </PopoverTrigger>
+            <PopoverContent bg="gray.700" color="yellow.400">
+              <PopoverArrow bg="gray.700" />
+              <PopoverCloseButton />
+              <PopoverBody fontWeight="400" fontSize="lg">
+                Paris, Europa, Chile, Canadá, China
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
